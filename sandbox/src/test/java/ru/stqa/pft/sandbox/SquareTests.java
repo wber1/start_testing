d75f1bd304d8c3c8890e3d3ded26ec3fcfc2f9c2
wber1 <40861793+wber1@users.noreply.github.com>
2018-07-31 13:28:48 +0300

    Подготовлен проект тестирования adressbook

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
@@ -7,6 +9,6 @@ public class SquareTests {
   @Test
   public void testArea(){
     Square s = new Square(5);
-    assert s.area() == 25;
+    Assert.assertEquals(s.area(), 25.0);
   }
 }
